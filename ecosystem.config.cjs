524e72ddfecdc5727c9040dfff88a0c3ae442e90
Pratik Phanase <107842052+pratikphanase@users.noreply.github.com>
2026-07-14 14:50:19 +0530

    Create ecosystem.config.cjs

---
+++ ecosystem.config.cjs
@@ -0,0 +1,21 @@
+module.exports = {
+
+  apps: [
+
+    {
+
+      name: "easyshop",
+
+      script: "npm",
+
+      args: "start",
+
+      instances: 1,
+
+      exec_mode: "fork"
+
+    }
+
+  ]
+
+}
